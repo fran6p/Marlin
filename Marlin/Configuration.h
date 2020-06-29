56db02175809d608a375e525c76c39b17a4c5dae
fran6p <32905274+fran6p@users.noreply.github.com>
2020-06-29 11:56:15 +0200

    Update Configuration.h
    Ajout suggestion David PHAM-VAN permettant le paramétrage automatique des «endstops» si Bltouch ou pas activé

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -654,12 +654,22 @@
 // Mechanical endstop with COM to ground and NC to Signal uses "false" here (most common setup).
 #define X_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
 #define Y_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
-#define Z_MIN_ENDSTOP_INVERTING true // Set to true to invert the logic of the endstop.
+//#define Z_MIN_ENDSTOP_INVERTING true // Set to true to invert the logic of the endstop. //Test 
 #define X_MAX_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
 #define Y_MAX_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
-#define Z_MAX_ENDSTOP_INVERTING true // Set to true to invert the logic of the endstop.
-#define Z_MIN_PROBE_ENDSTOP_INVERTING true // Set to true to invert the logic of the probe.
+//#define Z_MAX_ENDSTOP_INVERTING true // Set to true to invert the logic of the endstop. //Test
+//#define Z_MIN_PROBE_ENDSTOP_INVERTING true // Set to true to invert the logic of the probe. //Test
 
+// Test suggestion David PHAM-VAN
+#if ENABLED(BLTOUCH)
+  #define Z_MIN_ENDSTOP_INVERTING true
+  #define Z_MAX_ENDSTOP_INVERTING true
+  #define Z_MIN_PROBE_ENDSTOP_INVERTING true
+#else
+  #define Z_MIN_ENDSTOP_INVERTING false
+  #define Z_MAX_ENDSTOP_INVERTING false
+  #define Z_MIN_PROBE_ENDSTOP_INVERTING false
+#endif
 /**
  * Stepper Drivers
  *
