d2c64591a0daea27314404676cf631744bc1cf43
fran6p <32905274+fran6p@users.noreply.github.com>
2020-10-18 16:21:48 +0200

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -502,16 +502,10 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp  22.20
-    #define DEFAULT_Ki   1.08
-    #define DEFAULT_Kd 114.00
+    #define DEFAULT_Kp 19.47
+    #define DEFAULT_Ki 1.59
+    #define DEFAULT_Kd 59.40
   #endif
-  
-  // CR10-V2
-  #define DEFAULT_Kp 19.47
-  #define DEFAULT_Ki 1.59
-  #define DEFAULT_Kd 59.40
-
 #endif // PIDTEMP
 
 //===========================================================================
