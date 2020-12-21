738504866259967a248917f7647f838fe6b9a502
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2020-12-21 13:09:36 +0100

    Fix M600

--- Marlin/src/lcd/extui/lib/dgus_creality/DGUSScreenHandler.cpp
+++ Marlin/src/lcd/extui/lib/dgus_creality/DGUSScreenHandler.cpp
@@ -449,10 +449,6 @@ bool DGUSScreenHandler::HandlePendingUserConfirmation() {
 
   // Switch to the resume screen
   ScreenHandler.GotoScreen(DGUSLCD_SCREEN_PRINT_RUNNING);
-  while (!ScreenHandler.loop());  // Wait while anything is left to be sent
-
-  // Give DWIN a chance to catch up
-  delay(250);
 
   // We might be re-entrant here
   ExtUI::setUserConfirmed();
@@ -1130,7 +1126,7 @@ bool DGUSScreenHandler::loop() {
   const millis_t ms = millis();
   static millis_t next_event_ms = 0;
 
-  if (wait_for_user && current_screen != DGUSLCD_SCREEN_POPUP) {
+  if (ExtUI::isWaitingOnUser() && current_screen != DGUSLCD_SCREEN_POPUP) {
     // In some occassions the display needs more time to handle a screen change, for instance,
     // with ADVANCED_PAUSE_FEATURE, the calls to ExtUI::onUserConfirmRequired are quite fast
     DEBUG_ECHOLN("Nudging the display to update the current screen...");
